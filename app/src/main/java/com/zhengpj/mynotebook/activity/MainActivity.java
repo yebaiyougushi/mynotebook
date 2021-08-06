372e61b47c5eba8646057f41706454a0b662d168
zhengpeijian <zhengpeijian.5@bytedance.com>
2021-08-06 10:51:41 +0800

    foreach commit

--- app/src/main/java/com/zhengpj/mynotebook/activity/MainActivity.java
+++ app/src/main/java/com/zhengpj/mynotebook/activity/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
